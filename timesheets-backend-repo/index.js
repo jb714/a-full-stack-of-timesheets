1baee7013fc1588dcb2016926ea8120cbb19d5c4
Justin Bell <justinbell714@gmail.com>
2019-08-14 19:39:57 -0700

    Adding in DB connection and csv file

--- timesheets-backend-repo/index.js
+++ timesheets-backend-repo/index.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
 var cors = require('cors');
+var bodyParser     = require('body-parser');
+const mongoose = require('mongoose');
 
 var port = 8080;
 
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/test', {useNewUrlParser: true});
 
 app.listen(port, function(){
   console.log('Now up and running on port', port);
-})
+});
+
+mongoose.connection.on('connected', function(){
+  console.log('DB is up!!');
+});
